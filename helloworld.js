11d497beec3064ef7a6a6b4604f28d34bb59c251
aarohi.palkar <aarohi.palkar@gmail.com>
2017-09-04 14:14:38 -0400

    :inital commit

---
+++ helloworld.js
@@ -0,0 +1,46 @@
+$(document).ready(function()
+{
+	$('#tl').click(function()
+	{
+		$('#tl').css({'content':'url("top left.jpeg")'});
+	});
+	
+	$('#tm').click(function()
+	{
+		$('#tm').css({'content':'url("top mid.jpeg")'});
+	});
+	
+	$('#tr').click(function()
+	{
+		$('#tr').css({'content':'url("top right.jpeg")'});
+	});
+	
+	$('#ml').click(function()
+	{
+		$('#ml').css({'content':'url("mid left.jpeg")'});
+	});
+	
+	$('#mm').click(function()
+	{
+		$('#mm').css({'content':'url("mid mid.jpeg")'});
+	});
+	
+	$('#mr').click(function()
+	{
+		$('#mr').css({'content':'url("mid right.jpeg")'});
+		
+	});$('#bl').click(function()
+	{
+		$('#bl').css({'content':'url("bot left.jpeg")'});
+	});
+	
+	$('#bm').click(function()
+	{
+		$('#bm').css({'content':'url("bot mid.jpeg")'});
+	});
+	
+	$('#br').click(function()
+	{
+		$('#br').css({'content':'url("bot right.jpeg")'});
+	});
+});
